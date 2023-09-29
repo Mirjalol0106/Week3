23496a634463f7b18af1f43923e9523068ccb39a
Mirjalol0106 <91127402+Mirjalol0106@users.noreply.github.com>
2023-09-29 12:47:19 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,90 @@
+//
+//  main.cpp
+//  week2
+//
+//  Created by Mirjalol Choriyev on 29/09/23.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    
+//    const float pi = 3.14;
+//    pi = 4.56;
+//
+//    cout << pi <<endl;
+    
+//    int length = 10;
+//    float weight = 2.34;
+//    double num = 6.78765;
+//    char grade = 'A';
+//    string name = "Hello";
+//    bool is_better = true;
+//
+//
+//    cout << length << " " << weight << " " <<  num << " " << grade << " " << name << " " << is_better <<endl;
+//    cout << boolalpha << is_better << endl;
+    
+//    int a = 4, b = 5;
+//
+//    int x, y;
+//
+//    x = 10;
+//    y = 20;
+    
+//    int result;
+    
+    //addition
+//    cout << a + b << endl;
+//    cout << x+ a << endl;
+//
+    
+    //division
+//    cout << x/y <<endl;
+    
+    //Modulus
+    
+//    cout << x%a << endl;
+    
+    //++
+    
+//    x++;
+//    cout << x++ << endl;
+//    cout << x << endl;
+    
+//    problem 1
+    
+//    int int_num = 15;
+//    float flat_num = 4.3;
+//    double double_num = 76.3982;
+//    bool bool_num = false;
+//    char char_name = '$';
+//
+//    cout << int_num<< " " << flat_num << " " <<  double_num << " " << bool_num  << " " << char_name<<endl;
+    
+    
+//    problem 2
+    
+//    int a = 2;
+//    int b = 5;
+//
+//    cout << a * b <<endl;
+    
+//    problem 3
+    
+//    int a, b ;
+//    a = 23;
+//    b = 87;
+//
+//    cout << "Product:" << a << "*" << b << "=" << a * b <<endl;
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
